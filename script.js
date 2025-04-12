9189e510034afc4b350a46aec1bad39f2812ed98
Newb05aurus <198301838+Newb05aurus@users.noreply.github.com>
2025-04-12 00:02:37 -0400

    Add UI to rock-paper-scissors project.
    Added UI with buttons to rock-paper-scissors project.

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
 
-//TEST to insure script is linked correctly:
-// console.log("hello");
+const scoreContainerBox = document.querySelector("#scoreContainerBox");
+const playerScoreBox = document.querySelector("#playerScoreBox");
+const computerScoreBox = document.querySelector("#computerScoreBox");
+const finalResultsBox = document.querySelector("#finalResultsBox");
+
+const drawText = document.createElement("span");
+drawText.textContent = "\n This round was a draw.";
+
+const btnList = [rockBtn, paperBtn, scissorsBtn];
 
 
-// INITIALIZE getComputerChoice
-// CALL random method
+
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = "";
+
 function getComputerChoice() {
-    // CALL Math.random to find computers choice
-    let computerRandomChoice = Math.floor(Math.random() * 3);
-    // LOG choice to TEST it works
-    // console.log(computerRandomChoice);
-    // RETURN computers choice
+   
+    const computerRandomChoice = Math.floor(Math.random() * 3);
+   
     return computerRandomChoice;
 }
-// TEST getComputerChoice()
-// getComputerChoice();
 
-// INITIALIZE getHumanChoice
-function getHumanChoice() {
-    // collect user input (rock paper or scissor) 
-    let humanInput = prompt("What's your choice?");
-    // LOG humanInput 
-    console.log(humanInput);
-    // return choice
-    return humanInput;
-}
-// TEST getHumanChoice()
-/// getHumanChoice();
 
-// CALL getHumanChoice and getComputerChoice. Return human choice and computer choice.
+function playRound(humanChoice, computerChoice) { 
+    
+    
+    
+    if (scoreContainerBox.contains(drawText)){
+        
+        scoreContainerBox.removeChild(drawText);
+    }
 
-// CREATE playRound function
-function playRound(humanChoice, computerChoice) {
-    // CONVERT human input to lowercase 
-    // so comparisons are insensitive
-    // convert users choice to numerical value
+   
     if (humanChoice === `rock`) {
         humanChoice = 0;
     } else if (humanChoice === `paper`) {
@@ -42,93 +43,103 @@ function playRound(humanChoice, computerChoice) {
     } else if (humanChoice === `scissors`) {
         humanChoice = 2;
     }
-    // LOG CONVERSION to TEST it worked
-    // console.log(`testing the conversion: ${humanChoice}`);
-
-    // Find Winner
+    
     let winner;
-    // INCREMENT winners score by 1
-    // LOG with message saying who won
-
-    // IF human chose rock and computer chose rock
-        // Draw
-    // ELSE IF human chose rock and computer chose paper
-        // Computer wins
-    // ELSE IF human chose rock and computer chose scissors
-        // Human wins
-    // ELSE IF human chose paper and computer chose rock
-        // Human wins
-    // ELSE IF human chose paper and computer chose paper
-        // Draw
-    // ELSE IF human chose paper and computer chose scissors
-        // Computer wins
-    // ELSE IF human chose scissors and computer chose rock
-        // Computer wins
-    // ELSE IF human chose scissors and computer chose paper
-        // Human wins
-    // ELSE IF human chooses scissors and computer chose scissors
-        // Draw
+   
     if (humanChoice === 0 && computerChoice === 0) {
-        winner = "draw";
+    
+        draw = true;
+        scoreContainerBox.appendChild(drawText);
+
     } else if (humanChoice === 0 && computerChoice === 1) {
-        winner = "computer";
-        console.log(`Computer wins this round. Paper beats rock!`);
+        
+        computerScore += 1;
+        computerScoreBox.textContent = `Computer: ${computerScore}`;
+        
     } else if (humanChoice === 0 && computerChoice === 2) {
-        winner = "player";
-        console.log(`Player wins this round. Rock beats scissors!`);
+        
+        playerScore += 1;
+        playerScoreBox.textContent = `You: ${playerScore}`;
+        
     } else if (humanChoice === 1 && computerChoice === 0) {
-        winner = "player";
-        console.log(`Player wins this round. Paper beats rock!`);
+        
+        playerScore += 1;
+        playerScoreBox.textContent = `You: ${playerScore}`;
+        
     } else if (humanChoice === 1 && computerChoice === 1) {
-        winner = "draw";
+    
+        draw = true;
+        scoreContainerBox.appendChild(drawText);
+        
     } else if (humanChoice === 1 && computerChoice === 2) {
-        winner = "computer";
-        console.log(`Computer wins this round. Scissors beat paper!`);
+        
+        computerScore += 1;
+        computerScoreBox.textContent = `Computer: ${computerScore}`;
+        
     } else if (humanChoice === 2 && computerChoice === 0) {
-        winner = "computer";
-        console.log(`Computer wins this round. Rock beats scissors!`);
+        
+        computerScore += 1;
+        computerScoreBox.textContent = `Computer: ${computerScore}`;
+
     } else if (humanChoice === 2 && computerChoice === 1) {
-        winner = "player";
-        console.log(`Player wins this round. Scissors beat paper!`);
+        
+        playerScore += 1;
+        playerScoreBox.textContent = `You: ${playerScore}`;
+        
     } else if (humanChoice === 2 && computerChoice === 2) {
-        winner = "draw";
+    
+        draw = true;
+        scoreContainerBox.appendChild(drawText);
     }
-    // RETURN winner
-    return winner;
+    
+    checkScoreForWinner();
 }
 
-// INITIALIZE playGame
-function playGame() {
-    // create variables to store player scores
-    let playerScore = 0;
-    let computerScore = 0;
-    // call playRound 5 times to play 5 rounds
-    // WHILE playerScore or computerScore are less than 5
-        // CALL playRound() and store result
-        // IF winner is player INCREMENT playerScore 
-            // ELSE IF winner is computer INCREMENT computerScore
-           // ELSE IF winner is draw then BREAK
-    while (playerScore < 5 && computerScore < 5) {
-        let roundWinner = playRound(getHumanChoice().toLowerCase(), getComputerChoice() );
-        if (roundWinner === "player") {
-            playerScore++;
-            console.log(`Player score: ${playerScore}`);
-        } else if (roundWinner === "computer") {
-            computerScore++;
-            console.log(`Computer score: ${computerScore}`);
-        } else if (roundWinner === "draw") {
-            console.log(`Draw!`);
-            continue;
-        }
-        // IF player has 5 then player wins 
-        // ELSE IF computer has five computer wins
-        if (playerScore === 5) {
-            console.log(`You won! Scoreboard: ${playerScore}` + `-` + computerScore);
-            alert(`You won! Scoreboard: ${playerScore}` + `-` + computerScore);
-        } else if (computerScore === 5) {
-            console.log("Computer wins. Nice try!");
-        }
+function checkScoreForWinner() {
+
+    
+    
+    if (playerScore === 5) {
+        finalResultsBox.textContent = " You win!";
+    } else if (computerScore === 5) {
+        finalResultsBox.textContent = "Computer wins. Nice try!";
     }
 }
 
-playGame();
+for (let i = 0; i <= (btnList.length - 1); i++){
+    btnList[i].addEventListener("mouseenter", () =>{
+        btnList[i].style.border = "2px solid blue";
+    })
+}
+
+for (let i = 0; i <= (btnList.length - 1); i++){
+    btnList[i].addEventListener("mouseleave", () =>{
+        btnList[i].style.border = "";
+    })
+}
+
+
+
+rockBtn.addEventListener("click", () =>{
+   roundWinner = playRound("rock", getComputerChoice());
+   
+});
+
+paperBtn.addEventListener("click", () =>{
+    roundWinner = playRound("paper", getComputerChoice());
+    
+});
+
+scissorsBtn.addEventListener("click", () =>{
+    roundWinner = playRound("scissors", getComputerChoice());
+    
+});
+
+
+
+
+
+
+
+
+
